1e4fb0f1da2f0699a2eca210bf836c6b524dde30
Ian Lancaster <ianclancaster@gmail.com>
2017-01-04 22:09:33 -0700

    run redux g dumb

---
+++ src/components/dumbName.js
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react';
+
+const propTypes = {
+};
+
+class DumbName extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+DumbName.propTypes = propTypes;
+export default DumbName;
