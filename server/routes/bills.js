c400717f2a6fdc6737c42c8df2b91ca7232a9254
Ian Lancaster <ianclancaster@gmail.com>
2017-01-05 18:19:38 -0700

    create functioning bill name structured endpoint

--- server/routes/bills.js
+++ server/routes/bills.js
@@ -5,7 +5,10 @@ const router = express.Router()
 router.get('/bills', (req, res) => {
   fetch('https://congress.api.sunlightfoundation.com/bills')
   .then(response => response.json())
-  .then(json => console.log(json))
+  .then(data => ({
+    billNames: data.results.map((bill) => `${bill.official_title.slice(0, 80)}...`)
+  }))
+  .then(structuredData => res.json(structuredData))
   .catch(err => console.log(err))
 
   // turn into json
@@ -14,3 +17,10 @@ router.get('/bills', (req, res) => {
 })
 
 module.exports = router
+
+// var array = [];
+//
+// array.map(function (Value, Index, array) {
+//   //code block
+// }, thisArg);
+// });
