d790a280a6962a2d1d03f1112f3532fdcc2418a8
Ian Lancaster <ianclancaster@gmail.com>
2017-01-05 18:28:28 -0700

    clears boilerplate project

--- server/routes/bills.js
+++ server/routes/bills.js
@@ -6,21 +6,10 @@ router.get('/bills', (req, res) => {
   fetch('https://congress.api.sunlightfoundation.com/bills')
   .then(response => response.json())
   .then(data => ({
-    billNames: data.results.map((bill) => `${bill.official_title.slice(0, 80)}...`)
+    billNames: data.results.map(bill => `${bill.official_title.slice(0, 80)}...`)
   }))
   .then(structuredData => res.json(structuredData))
-  .catch(err => console.log(err))
-
-  // turn into json
-  // structure Object
-  // respond with structured object
+  .catch(err => res.json(err))
 })
 
 module.exports = router
-
-// var array = [];
-//
-// array.map(function (Value, Index, array) {
-//   //code block
-// }, thisArg);
-// });
