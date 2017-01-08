628515dec0770289ca585025db7cd30e6fe4cbb0
Ian Lancaster <ianclancaster@gmail.com>
2017-01-08 02:22:10 -0700

    add index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -22,6 +22,7 @@ export const createRoutes = (store) => {
   const routes = {
     path: '/',
     component: Iphone,
+    indexRoute: { onEnter: (nextState, replace) => replace('/bills') },
     getChildRoutes (location, next) {
       require.ensure([], (require) => {
         next(null, [
