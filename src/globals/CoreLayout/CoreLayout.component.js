fecc2763080267b3df68de553ed2c03abae18987
Ian Lancaster <ianclancaster@gmail.com>
2017-01-08 05:47:45 -0700

    auto hide top nav on scroll down

--- src/globals/CoreLayout/CoreLayout.component.js
+++ src/globals/CoreLayout/CoreLayout.component.js
@@ -5,26 +5,46 @@ import classes from './CoreLayout.styles.scss'
 class CoreLayout extends Component {
   constructor () {
     super()
-    this.state = {}
+    this.state = {
+      mainHeight: 400,
+      scrollTop: 0,
+      showNav: true
+    }
   }
   componentDidMount () {
     this.setContentViewportHeight()
+    this.watchScrollActivity()
   }
   setContentViewportHeight () {
+    const header = document.getElementById('header')
     const coreLayoutHeight = document.getElementById('coreLayout').offsetHeight
-    const headerHeight = document.getElementById('header').offsetHeight
+    const headerHeight = header ? header.offsetHeight : 0
     this.setState({ mainHeight: coreLayoutHeight - headerHeight })
 
     window.onresize = () => {
       this.setState({ mainHeight: coreLayoutHeight - headerHeight })
     }
   }
+  watchScrollActivity () {
+    const main = document.getElementById('main')
+    main.onscroll = (e) => {
+      if (this.state.scrollTop < e.srcElement.scrollTop) {
+        this.setState({ showNav: false })
+      } else {
+        this.setState({ showNav: true })
+      }
+      this.setContentViewportHeight()
+      this.setState({ scrollTop: e.srcElement.scrollTop })
+    }
+  }
   render () {
+    const { children } = this.props
+    const { showNav, mainHeight } = this.state
     return (
       <div id={'coreLayout'} className={classes.coreLayout}>
-        <Header />
-        <main style={{ height: `${this.state.mainHeight}px` }}>
-          {this.props.children}
+        {showNav && (<Header />)}
+        <main id={'main'} style={{ height: `${mainHeight}px` }}>
+          {children}
         </main>
       </div>
     )
