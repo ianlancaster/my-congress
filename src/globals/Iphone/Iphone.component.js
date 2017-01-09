8fa14d031a32a1ce80955269636a669b5e0dc3f3
Ian Lancaster <ianclancaster@gmail.com>
2017-01-09 16:21:58 -0700

    updates iphone image to be pulled from sass

--- src/globals/Iphone/Iphone.component.js
+++ src/globals/Iphone/Iphone.component.js
@@ -1,11 +1,10 @@
 import React, { Component, PropTypes } from 'react'
 import classes from './Iphone.styles.scss'
-import iphoneImage from './assets/iphone6.png'
 import CoreLayout from 'globals/CoreLayout'
 
 const Iphone = ({ children }) => {
   return (
-    <div style={{ background: `url(${iphoneImage}) no-repeat` }} className={classes.iphone}>
+    <div className={classes.iphone}>
       <div className={classes.outlet}>
         <CoreLayout>
           {children}
--- src/routes/Bills/index.js
+++ src/routes/Bills/index.js
@@ -1,16 +1,3 @@
-// import React from 'react'
-// import { Route } from 'react-router'
-// import Bills from './Bills.component'
-// import BillsContainer from './Bills.container'
-//
-// const Routes = () => {
-//   return (
-//     <Route path='/bills' component={BillsContainer} />
-//   )
-// }
-//
-// export default Routes
-
 import { injectReducer } from 'store/reducers'
 
 export default (store) => ({
